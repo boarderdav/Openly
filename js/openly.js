d657c72d501423c4437d0abf5d9e2a3a7cd091aa
David Jenkins <djenk008@odu.edu>
2015-03-30 01:15:41 -0400

    first publish syncing everything
    first publish and syncing everything to github

---
+++ js/openly.js
@@ -0,0 +1,42 @@
+// JavaScript Document
+
+$(document).ready(function() {
+	
+	$('#facebook').on('mouseenter', function(){
+		$(this).animate({'opacity': '0.7'}, 'fast');
+	});
+	$('#facebook').on('mouseleave', function(){
+		$(this).animate({'opacity': '1'}, 'fast');
+	});
+	
+	$('#twitter').on('mouseenter', function(){
+		$(this).animate({'opacity': '0.7'}, 'fast');
+	});
+	$('#twitter').on('mouseleave', function(){
+		$(this).animate({'opacity': '1'}, 'fast');
+	});
+	
+	$('#instagram').on('mouseenter', function(){
+		$(this).animate({'opacity': '0.7'}, 'fast');
+	});
+	$('#instagram').on('mouseleave', function(){
+		$(this).animate({'opacity': '1'}, 'fast');
+	});
+	
+	$('#facebook1').on('mouseenter', function(){
+		$(this).animate({'opacity': '0.7'}, 'fast');
+	});
+	$('#facebook1').on('mouseleave', function(){
+		$(this).animate({'opacity': '1'}, 'fast');
+	});
+	
+	
+	
+//	$('social').on('mouseenter', function(){
+//		$(this).animate({'opacity': '0.7'}, 'fast');
+//	});
+//	$('social').on('mouseleave', function(){
+//		$(this).animate({'opacity': '1'}, 'fast');
+//	});
+	
+});
